f3c08a6dfaafbe0d65716b055c6f1178b49b78e5
Kyle-Ferguson <kyle.ferguson@knights.ucf.edu>
2017-02-06 14:04:48 -0500

    Delete Rotate.hs

--- Rotate.hs
+++
@@ -1,9 +0,0 @@
-module Rotate where
-import Amount
-
-rotate :: Amount -> (a,a,a,a) -> (a,a,a,a)
-
-rotate Zero (w,x,y,z) = (w,x,y,z)
-rotate One (w,x,y,z) = (z,w,x,y)
-rotate Two (w,x,y,z) = (y,z,w,x)
-rotate Three (w,x,y,z) = (x,y,z,w)
